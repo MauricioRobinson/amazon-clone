a4db95bfc7746d4ba8fc56c196dea9889e5a4699
Mauricio Robinson <marsjr97@gmail.com>
2022-12-17 14:59:58 -0500

    Setting up the store and reducers for redux

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+  mode: "jit",
+  purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
+
